cef0e277dc34461dd0065b659929a75d8608a92c
lxz07 <liuxiongzhuo@163.com>
2026-09-09 14:31:16 +0800

    dns tls 使用 readexactly

--- pybox/dns/servers/tls.py
+++ pybox/dns/servers/tls.py
@@ -2,13 +2,7 @@ import asyncio
 import ipaddress
 import ssl
 
-from ...common.address import (
-    DOMAIN_Address,
-    IPV4_Address,
-    IPV6_Address,
-    host_port_to_addr,
-)
-from ...common.network import connect_address, connnect_ip
+from ...common.network import connnect_ip
 from .server import DnsServer
 
 
@@ -25,25 +19,33 @@ class TlsDnsServer(DnsServer):
         self.time_out = 3
         self.server_hostname = server_hostname
         self.insecure = insecure
+        self.reader: asyncio.StreamReader | None = None
+        self.writer: asyncio.StreamWriter | None = None
 
     async def query(self, request: bytes) -> bytes:
+        if not self.reader or not self.writer:
+            await self.build_connect()
+        assert self.writer and self.reader
+        self.writer.write(len(request).to_bytes(2, "big") + request)
+        await self.writer.drain()
+        try:
+            length_bytes = await asyncio.wait_for(
+                self.reader.readexactly(2), self.time_out
+            )
+            length = int.from_bytes(length_bytes, "big")
+            data = await asyncio.wait_for(
+                self.reader.readexactly(length), self.time_out
+            )
+        except asyncio.TimeoutError:
+            raise RuntimeError("tls dns query timeout")
+        return data
+
+    async def build_connect(self):
         context = ssl.create_default_context()
         if self.insecure:
             context.check_hostname = False
             context.verify_mode = ssl.CERT_NONE
         sock = await connnect_ip(self.server, self.server_port)
-        reader, writer = await asyncio.open_connection(
+        self.reader, self.writer = await asyncio.open_connection(
             sock=sock, ssl=context, server_hostname=self.server_hostname
         )
-        writer.write(len(request).to_bytes(2, "big") + request)
-        await writer.drain()
-        try:
-            try:
-                res = await asyncio.wait_for(reader.read(4096), self.time_out)
-            except asyncio.TimeoutError:
-                raise RuntimeError("tls dns query timeout")
-            length = int.from_bytes(res[:2], "big")
-            return res[2 : 2 + length]
-        finally:
-            writer.close()
-            await writer.wait_closed()
